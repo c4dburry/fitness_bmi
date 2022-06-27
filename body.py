eb1998975c1d7276b83f8204641728875129b207
c4dburry <keyser.mathis@gmail.com>
2022-06-27 20:13:48 +0200

    init project

---
+++ body.py
@@ -0,0 +1,21 @@
+#!python3
+###written by c4dburry
+
+# BMI Rechner
+# Formel
+# BMI = Körpergewicht : (Körpergröße)²
+
+#Eingaben
+print("Bitte geben Sie ihr Gewicht ein: ")
+bodyweight=float(input())
+print("Bitte geben Sie ihre Körpergröße in cm an:")
+bodysize=float(input())
+bodysize=bodysize/100
+
+
+
+#Berechnung
+bmi = bodyweight / (bodysize**2 )
+format_bmi = "{:.2f}".format(bmi)
+
+print(format_bmi) 
